d5548da413e5f0b5d7c54b07f325112e41660553
Nguyen, Phuoc <phuoc.nguyen2@hrs.com>
2024-12-02 10:54:44 +0700

    add version

---
+++ lib/src/loading_manager.dart
@@ -0,0 +1,4 @@
+import 'package:inherited_rxdart/inherited_rxdart.dart';
+import 'package:loading_manager/src/class_loading_state.dart';
+
+class LoadingManager extends BaseViewModel<LoadingState> {}
